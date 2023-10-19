cb8dafa8f99c0d09e431c496cae9018ea5756507
Sahil18718 <119488054+Sahil18718@users.noreply.github.com>
2023-10-19 16:02:25 +0530

    project 4

---
+++ 4project/routes/auth.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+
+router.post('/signup', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    
+    // Check if user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: 'User already exists.' });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user
+    const newUser = new User({
+      email,
+      password: hashedPassword
+    });
+    
+    await newUser.save();
+
+    // Send a welcome email (you need to implement this)
+
+    res.status(201).json({ message: 'User created successfully.' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+module.exports = router;
