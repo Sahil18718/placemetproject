c193cd4676697d166c71164ec8d239ec8609d6ea
Sahil18718 <119488054+Sahil18718@users.noreply.github.com>
2023-10-20 22:22:08 +0530

    Update index.js

--- 4project/index.js
+++ 4project/index.js
@@ -2,16 +2,14 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-// Configure Express and middleware
 app.use(express.json());
 
-// Connect to MongoDB
 mongoose.connect('mongodb+srv://sahil:sahilmalviya@cluster0.hhowf26.mongodb.net/unit5final?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
-// Routes
+
 const authRoutes = require('./routes/auth');
 app.use('/api', authRoutes);
 
