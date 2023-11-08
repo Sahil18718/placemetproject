33c931e44f9e9a0f463f42cdf0a57027343d986c
Sahil18718 <119488054+Sahil18718@users.noreply.github.com>
2023-11-08 20:30:29 +0530

    Update index.js

--- 7projecgt/index.js
+++ 7projecgt/index.js
@@ -107,7 +107,7 @@ app.get('/api/users/details', validateToken, async (req, res) => {
   });
 });
 
-// Start the server
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
